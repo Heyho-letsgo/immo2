51a10bf26568bf91c7d5b6f236f617f7edf040f3
Baudin-Laurencin <abaudin@gmail.com>
2019-01-29 11:36:49 +0000

    suppression de  deux migrations

--- db/migrate/20190128114131_add_timestamp_to_user.rb
+++
@@ -1,8 +0,0 @@
-=begin
-class AddTimestampToUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :created_at, :datetime
-    remove_column :users, :updated_at, :datetime
-  end
-end
-=end
--- db/migrate/20190128120111_remove_columnsdate.rb
+++
@@ -1,8 +0,0 @@
-=begin
-class RemoveColumnsdate < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users,:created_at
-    remove_column :users,:updated_at
-  end
-end
-=end

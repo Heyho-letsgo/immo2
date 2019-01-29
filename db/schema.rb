4fdd2a7079bcbb5657d6aa9be7ac99abc62d9f47
Baudin-Laurencin <abaudin@gmail.com>
2019-01-29 15:45:32 +0000

    suppression de false àdatecreate et update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190128131908) do
+ActiveRecord::Schema.define(version: 20190129153630) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.string "password_confirmation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

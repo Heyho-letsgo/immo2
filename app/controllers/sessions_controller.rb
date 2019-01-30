e03ed81bea4858b59d8464087826bd2d5f17d651
Baudin-Laurencin <abaudin@gmail.com>
2019-01-30 14:48:56 +0000

    suppression des boutons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
       log_in user
-      flash[:success] = 'You are now logged in :-)'
       redirect_to user
     else
-      # Create an error message.
-      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129153630) do
+ActiveRecord::Schema.define(version: 20190130133637) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190129153630) do
     t.string "password_confirmation"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
